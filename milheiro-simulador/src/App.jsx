9bd44059d699a9c3899aa82d31241c22d817bd7a
gabiduartte <gabiduarte723@gmail.com>
2023-09-22 10:31:21 -0300

    -milhas-

--- milheiro-simulador/src/App.jsx
+++ milheiro-simulador/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './css/App.css'
 import CalcularMilheiroReal from './components/calculo-milhas';
 
 export default function App() {
