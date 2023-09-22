9f46a68cadb2e4fd71351fe2f573887805dc86e4
gabiduartte <gabiduarte723@gmail.com>
2023-09-22 10:44:02 -0300

    -milhas-

---
+++ milheiro-simulador/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css'
+
+const App = () => {
+  
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="dark-background"> 
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
