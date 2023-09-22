a0602bdb4a5bdd3eae0e517922116a92a9b540d3
gabiduartte <gabiduarte723@gmail.com>
2023-09-22 10:46:31 -0300

    -milhas-

--- milheiro-simulador/src/index.jsx
+++ milheiro-simulador/src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css'
-
-const App = () => {
-  
-};
+import './css/index.css';
+import App from './App'; 
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="dark-background"> 
-      <App />
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
