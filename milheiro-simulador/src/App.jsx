589515a81e7e8e3a5c2786c82662120d1cc914df
gabiduartte <gabiduarte723@gmail.com>
2023-09-22 10:27:51 -0300

    milhas

--- milheiro-simulador/src/App.jsx
+++ milheiro-simulador/src/App.jsx
@@ -5,6 +5,7 @@ import CalcularMilheiroReal from './components/calculo-milhas';
 export default function App() {
   const [currentPage, setCurrentPage] = useState('home');
   const [calculatedValues, setCalculatedValues] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
   function handleNextClick() {
     setCurrentPage('calculo');
@@ -16,27 +17,41 @@ export default function App() {
 
   function handleCalculated(values) {
     setCalculatedValues(values);
-    setCurrentPage('home'); 
+    setCurrentPage('home');
+  }
+
+  function handleLogin() {
+    setIsLoggedIn(true);
+  }
+
+  function handleLogout() {
+    setIsLoggedIn(false);
   }
 
   return (
     <div className={`App ${currentPage === 'home' ? 'home-page' : 'calculo-page'}`}>
       <div className="calculator-box">
-        {currentPage === 'home' && (
+        {isLoggedIn ? (
           <div>
             <h1>Simulador de Milheiro</h1>
             <button onClick={handleNextClick}>Next</button>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <h1>Please Log In</h1>
+            <button onClick={handleLogin}>Login</button>
           </div>
         )}
 
-        {currentPage === 'calculo' && (
+        {isLoggedIn && currentPage === 'calculo' && (
           <CalcularMilheiroReal
             onCalculated={handleCalculated}
             onBackToHome={handleBackToHome}
           />
         )}
 
-        {currentPage === 'home' && calculatedValues && (
+        {isLoggedIn && currentPage === 'home' && calculatedValues && (
           <div>
             <p>Valores calculados:</p>
             <p>Milhas: {calculatedValues.milhas}</p>
@@ -47,3 +62,6 @@ export default function App() {
     </div>
   );
 }
+
+
+
